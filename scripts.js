aa8c9b7e435ed24de9157c2b308973470f960d10
Brian Zimorowicz <zims@nestofsaws.com>
2014-11-10 08:46:34 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,7 +8,9 @@ function openClosed(){
 	var hour = now.getHours();
 	var day = now.getDay();
 	
-	if (day >= 1 && day <= 5 && hour < 8 && hour > 18){
+	
+	
+	if (day >= 1 && day <= 5 && hour >= 8 && hour < 18){
 		//alert('open');
 		$('#openClose').html('<p class="centereddd">We are open until 6pm today. Give us a call or schedule an appointment online now!</p>');
 		
