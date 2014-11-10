dcd7982e359a3686815ab0261b80722c44669583
Brian Zimorowicz <zims@nestofsaws.com>
2014-11-10 07:58:24 -0500

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	openClosed();
+}); //end ready
+
+function openClosed(){
+	var now = new Date();
+
+	var hour = now.getHours();
+	var day = now.getDay();
+	
+	if (day >= 1 && day <= 5 && hour < 8 && hour > 18){
+		//alert('open');
+		$('#openClose').html('<p class="centereddd">We are open until 6pm today. Give us a call or schedule an appointment online now!</p>');
+		
+	} else {
+		//alert('not open');
+		$('#openClose').html('<p class="centereddd">Sorry, we are closed right now. We will reopen Monday at 8am. Give us a call then or schedule an appointment online now!</p>');
+		
+	}
+	//$(#openClosed)
+
+};
