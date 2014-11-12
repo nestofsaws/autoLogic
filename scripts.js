ab3a73af8ff0118a00ec361b34b5e92f891bf6ca
Brian Zimorowicz <zims@nestofsaws.com>
2014-11-12 08:56:51 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,24 +1,28 @@
 $(document).ready(function(){
 	openClosed();
+	twoLink();
 }); //end ready
 
 function openClosed(){
 	var now = new Date();
-
+	var hour = now.getHours();
 	var hour = now.getHours();
 	var day = now.getDay();
-	
-	
-	
-	if (day >= 1 && day <= 5 && hour >= 8 && hour < 18){
-		//alert('open');
-		$('#openClose').html('<p class="centereddd">We are open until 6pm today. Give us a call or schedule an appointment online now!</p>');
+		
+	if (hour >= 8 && hour < 18 && day >= 1 && day <= 5){
+		$('#openClose').html('<p class="centereddd">We are open until 6pm today. <br/> Give us a call or schedule an appointment online now!</p>');
 		
 	} else {
-		//alert('not open');
-		$('#openClose').html('<p class="centereddd">Sorry, we are closed right now. We will reopen Monday at 8am. Give us a call then or schedule an appointment online now!</p>');
+		$('#openClose').html('<p class="centereddd">Sorry, we are closed right now.<br/>Give us a call or schedule an appointment online now!</p>'); 
 		
 	}
-	//$(#openClosed)
 
-};
+}; //end openClosed
+
+function twoLink (){
+   $("#twoLink").click(function(){
+   window.open("http://www.google.com",'_blank');
+   window.open("http://www.p3php.in",'_blank');
+});
+
+};// end twoLink
